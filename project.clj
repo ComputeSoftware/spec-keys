42cd60436669e7c84b70a67a5881ff86c65d6fb2
Kenny Williams <kenny@computesoftware.com>
2018-06-15 13:21:49 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compute/spec-keys "0.1.0"
+(defproject compute/spec-keys "0.1.1-SNAPSHOT"
   :description "Get the keys from a Spec."
   :url "https://github.com/ComputeSoftware/spec-keys"
   :license {:name "Eclipse Public License"
